069b5923c51e8435aa81ae716b9edd32fcca76e3
asmacorporate-rgb <asmacorporate1@gmail.com>
2025-09-26 02:32:21 +0600

    Add and remove from the cart

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -3,17 +3,24 @@
 import { test, expect } from '@playwright/test';
 
 test.use({
-  launchOptions: {
-    slowMo: 1000 // adds 1 second delay between actions
-  }
+    launchOptions: {
+        slowMo: 1000 // adds 1 second delay between actions
+    }
 });
 
-test('test', async({page}) => {
-// login to the site
+test('test', async ({ page }) => {
+    // login to the site
     await page.goto('https://www.saucedemo.com/');
     await expect(page.getByText('Swag Labs')).toBeVisible();
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    await expect(page.locator('[data-test="shopping-cart-link"]')).toHaveText('2');
+    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+    await expect(page.locator('[data-test="shopping-cart-link"]')).toHaveText('1');
+  
 })
