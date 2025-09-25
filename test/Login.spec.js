90aa713e4dc8cb1f015731bd4069f8eaa60b1585
asmacorporate-rgb <asmacorporate1@gmail.com>
2025-09-26 02:17:57 +0600

    Install Playwright

---
+++ test/Login.spec.js
@@ -0,0 +1,8 @@
+
+//A repository to store automation test scripts using the tool playwright
+import { test, expect } from '@playwright/test';
+
+test('test', async({page}) => {
+
+    await page.goto('https://www.saucedemo.com/');
+})
