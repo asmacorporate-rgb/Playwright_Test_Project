fd10fedc7b642261611dc44dd6d84c32fc48e01d
asmacorporate-rgb <asmacorporate1@gmail.com>
2025-09-26 02:37:18 +0600

    use beforeEach

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -1,5 +1,3 @@
-
-//A repository to store automation test scripts using the tool playwright
 import { test, expect } from '@playwright/test';
 
 // Constants
@@ -8,24 +6,34 @@ const USERNAME = 'standard_user';
 const PASSWORD = 'secret_sauce';
 
 test.use({
-    launchOptions: {
-        slowMo: 2000 // adds 1 second delay between actions
-    }
+  launchOptions: {
+    slowMo: 2000 // slows execution for visual checking
+  }
 });
 
-test('test a website', async ({ page }) => {
-    // login to the site
+// Run before each test Login
+test.beforeEach(async ({ page }) => {
   await page.goto(BASE_URL);
   await expect(page.getByText('Swag Labs')).toBeVisible();
   await page.getByPlaceholder('Username').fill(USERNAME);
   await page.getByPlaceholder('Password').fill(PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+});
 
-    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-    await expect(page.locator('[data-test="shopping-cart-link"]')).toHaveText('2');
-    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
-    await expect(page.locator('[data-test="shopping-cart-link"]')).toHaveText('1');
-  
-})
+
+// Add/Remove items from cart
+test('Add and remove items from cart', async ({ page }) => {
+
+  // Add items
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  // check if the items added to the cart
+  await expect(page.locator('[data-test="shopping-cart-link"]')).toHaveText('2');
+
+  // Remove item
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+   // check if the removed items updated in the cart
+  await expect(page.locator('[data-test="shopping-cart-link"]')).toHaveText('1');
+
+  });
