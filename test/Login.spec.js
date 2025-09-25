3b304ff7f86c26472372b020806ff576912177c5
asmacorporate-rgb <asmacorporate1@gmail.com>
2025-09-26 02:26:34 +0600

    Set-up Login

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -3,6 +3,11 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async({page}) => {
-// 
+// login to the site
     await page.goto('https://www.saucedemo.com/');
+    await expect(page.getByText('Swag Labs')).toBeVisible();
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 })
